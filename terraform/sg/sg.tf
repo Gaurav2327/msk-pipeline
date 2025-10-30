3c0c3302457e0cad374ed206d7dd12347066e0d8
Gaurav Dhapola <gaurav.dhapola2327@gmail.com>
2025-10-30 12:17:42 +0530

    format

--- terraform/sg/sg.tf
+++ terraform/sg/sg.tf
@@ -5,30 +5,30 @@ resource "aws_security_group" "rds_security_group" {
 
   # RDS allows inbound MySQL traffic from Connector 
   ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    security_groups = [ aws_security_group.connector_security_group.id ]
-    }
-
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    security_groups = [ data.aws_security_group.vpc_sg.id ]
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.connector_security_group.id]
   }
 
-    # Allow all outbound traffic
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [data.aws_security_group.vpc_sg.id]
+  }
+
+  # Allow all outbound traffic
   egress {
     from_port   = 0
-    to_port = 0
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = []
   }
 
-    tags = merge(local.default_tags,{
-      name = "rds-sg"
-    })
+  tags = merge(local.default_tags, {
+    name = "rds-sg"
+  })
 }
 
 ################################################
@@ -42,30 +42,30 @@ resource "aws_security_group" "msk_security_group" {
 
   # MSK allows inbound traffic from Connectors
   ingress {
-    from_port   = 9092
-    to_port     = 9092
-    protocol    = "tcp"
-    security_groups = [ aws_security_group.connector_security_group.id ]
-    }
-
-  ingress {
-    from_port   = 9092
-    to_port     = 9092
-    protocol    = "tcp"
-    security_groups = [ data.aws_security_group.vpc_sg.id ]
+    from_port       = 9092
+    to_port         = 9092
+    protocol        = "tcp"
+    security_groups = [aws_security_group.connector_security_group.id]
   }
 
-    # Allow all outbound traffic
+  ingress {
+    from_port       = 9092
+    to_port         = 9092
+    protocol        = "tcp"
+    security_groups = [data.aws_security_group.vpc_sg.id]
+  }
+
+  # Allow all outbound traffic
   egress {
     from_port   = 0
-    to_port = 0
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = []
   }
 
-    tags = merge(local.default_tags, {
-      name = "msk-sg"
-    })
+  tags = merge(local.default_tags, {
+    name = "msk-sg"
+  })
 }
 
 ################################################
@@ -77,16 +77,16 @@ resource "aws_security_group" "connector_security_group" {
   description = "Security group for MSK Connectors"
   vpc_id      = data.aws_vpc.default_vpc.id
 
-    # Allow all outbound traffic
+  # Allow all outbound traffic
   egress {
     from_port   = 0
-    to_port = 0
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-    tags = merge(local.default_tags,{
-      name = "connector-sg"
-    })
-  
+  tags = merge(local.default_tags, {
+    name = "connector-sg"
+  })
+
 }
