b97be3596aec832258577700b5fd7610282f1a70
Gaurav Dhapola <79783906+Gaurav2327@users.noreply.github.com>
2025-10-30 16:41:39 +0530

    rds-cluster (#8)

---
+++ terraform/rds/data.tf
@@ -0,0 +1,31 @@
+data "aws_availability_zones" "available" {}
+
+data "aws_vpc" "default_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["default-vpc"]
+  }
+}
+
+# Fetch all subnets in the VPC
+data "aws_subnets" "all_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default_vpc.id]
+  }
+}
+
+# Fetch public subnets based on them being tagged as "public"
+data "aws_subnets" "public_subnets" {
+  filter {
+    name   = "tag:Name"
+    values = ["public-subnet-*"]
+  }
+}
+
+data "aws_security_group" "rds_sg" {
+    filter {
+        name   = "group-name"
+        values = ["db-sg"]
+    }
+}
---
+++ terraform/rds/rds.tf
@@ -0,0 +1,60 @@
+locals {
+  cluster_group_params = [
+    {
+        name = "binlog_format"
+        value = "row"
+        apply_method = "pending-reboot"
+    },
+    {
+        name = "binlog_row_image"
+        value = "FULL"
+        apply_method = "pending-reboot"
+    },
+  ]
+  instance_group_params = []
+}
+
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = var.rds_subnet_group_name
+  subnet_ids = [ data.aws_subnets.public_subnets.ids[0], data.aws_subnets.public_subnets.ids[1] ]
+
+  tags = {
+    Name = var.rds_subnet_group_name
+  }
+}
+
+module "rds_mysql_db" {
+    source = "terraform-aws-modules/rds-aurora/aws"
+    version = "9.16.1"
+    name = var.rds_cluster_name
+    engine = var.db_engine_type
+    engine_version = var.db_engine_version
+    master_username = var.db_master_username
+    master_password = var.db_master_password
+    instances = {
+        main = {
+            instance_class = var.db_instance_type
+            identifier     = "${var.rds_cluster_instance_name}-${var.rds_identifier_suffix}"
+        }
+    }
+    vpc_id = data.aws_vpc.default_vpc.id
+    db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
+    vpc_security_group_ids = [data.aws_security_group.rds_sg.id]
+    apply_immediately = true
+    skip_final_snapshot = true
+    create_db_cluster_parameter_group      = true
+    db_cluster_parameter_group_name        = var.db_cluster_parameter_group_name
+    db_cluster_parameter_group_description = "Custom parameter group for MySQL RDS cluster"
+    db_cluster_parameter_group_parameters = local.cluster_group_params
+    db_cluster_parameter_group_family = var.db_cluster_parameter_group_family
+    create_db_parameter_group = true
+    db_parameter_group_name   = var.db_parameter_group_name
+    db_parameter_group_description = "Custom parameter group for MySQL RDS instances"
+    db_parameter_group_parameters = local.instance_group_params
+    db_parameter_group_family = var.db_parameter_group_family
+    publicly_accessible = true
+    tags     = merge(local.default_tags, 
+    { 
+        Name = var.rds_cluster_name
+    })    
+}
